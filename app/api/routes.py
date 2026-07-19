676a46e94f0c211ec946a33c9d07f695d2c287af
SivRang <rangakrishnan.sivasailam@gmail.com>
2026-07-20 00:05:49 +0530

    Update routes.py

--- app/api/routes.py
+++ app/api/routes.py
@@ -5,7 +5,6 @@ from fastapi import APIRouter
 from langchain_core.messages import HumanMessage
 
 from app.graph.workflow import graph
-
 from app.models.schemas import ChatRequest
 
 router = APIRouter()
@@ -16,23 +15,13 @@ def chat(request: ChatRequest):
 
     session_id = request.session_id or str(uuid4())
 
-    result = graph.invoke(
-        state,
-        config={
-            "configurable": {
-                "thread_id": session_id
-            }
-        }
-    )
-
-    print("Returned State:")
-    print(result)
-
-    #session_id = request.session_id or str(uuid4())
-
+    # First request in conversation
     if request.session_id is None:
+
         state = {
-            "messages": [HumanMessage(content=request.message)],
+            "messages": [
+                HumanMessage(content=request.message)
+            ],
             "session_id": session_id,
             "router_vendor": "",
             "router_model": "",
@@ -47,23 +36,39 @@ def chat(request: ChatRequest):
             "in_scope": True,
             "greeted": False,
         }
+
+    # Existing conversation
     else:
+
         state = {
-            "messages": [HumanMessage(content=request.message)]
+            "messages": [
+                HumanMessage(content=request.message)
+            ]
         }
+
+    config = {
+        "configurable": {
+            "thread_id": session_id
+        }
+    }
 
     result = graph.invoke(
         state,
         config=config
     )
 
+    print("=" * 60)
+    print("Returned State")
+    print(result)
+    print("=" * 60)
+
     return {
         "session_id": session_id,
         "messages": [
             m.content
-            for m in result["messages"]
+            for m in result.get("messages", [])
         ],
-        "resolved": result["resolved"],
-        "next_action": result["next_action"],
-        "question_count": result["question_count"]
+        "resolved": result.get("resolved", False),
+        "next_action": result.get("next_action", "ASK"),
+        "question_count": result.get("question_count", 0)
     }
